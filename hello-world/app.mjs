8a8b2173784a7241ebdf1d2bf47a5a705aa07dad
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-07-17 02:22:20 +0530

    [del] useless code

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -1,16 +1,3 @@
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
-
 let count = 0;
 
 export const lambdaHandler = async (event, context) => {
@@ -27,6 +14,4 @@ export const lambdaHandler = async (event, context) => {
         console.log(err);
         return err;
     }
-
-    return response
 };
